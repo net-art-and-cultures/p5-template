fbccb6146610c8d0df95e3b6e1f0e419b782a470
Jieun LEE <j.i.v.e.l.y@Jieuns-MacBook-Pro.local>
2019-10-14 01:15:56 -0500

    commit all

---
+++ Jieun/sketch.js
@@ -0,0 +1,16 @@
+let box;
+
+function setup() {
+  createCanvas(400, 400);
+
+function draw() {
+  //color the background
+  background('White');
+
+  box.x = 150;
+  box.y = 300;
+  box.size = 75;
+  box.color = 'papayawhip';
+  box.spin = 45;
+  box.show();
+}
